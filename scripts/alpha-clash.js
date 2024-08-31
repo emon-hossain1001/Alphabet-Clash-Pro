2f404255cb1aac80a38d72bb1d63850044f4ab03
emon-hossain1001 <mrx00001001@gmail.com>
2024-08-31 10:46:04 +0600

    create function who generate a random alphabet

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,7 +1,12 @@
+function continueGame(){
+    const alphabet = getRandomAlphabet();
+    console.log(alphabet);
+}
+
 function play(){
     //hide home section
     hideELementById('home');
-
     // show playground section
     showElementById('playground');
+    continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -5,3 +5,16 @@ function hideELementById(ID){
 function showElementById(ID){
     document.getElementById(ID).classList.remove('hidden');
 }
+
+function getRandomAlphabet(){
+    // get or create a alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    // get a random index between 0-25
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+    
+    const alphabet = alphabets[index];
+    return alphabet;
+}
